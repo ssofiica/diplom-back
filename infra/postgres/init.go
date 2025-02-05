0ef6dcea01308003229d143a3051a3b2cd14dddc
ssofiica <s.valova2016@yandex.ru>
2025-02-05 14:24:55 +0300

    BACK-1: настроить проект

--- infra/postgres/init.go
+++ infra/postgres/init.go
@@ -1,1 +1,16 @@
 package postgres
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func Init(connUrl string) *pgxpool.Pool {
+	db, err := pgxpool.New(context.Background(), connUrl)
+	if err != nil {
+		fmt.Println("error wih db", err)
+	}
+	return db
+}
